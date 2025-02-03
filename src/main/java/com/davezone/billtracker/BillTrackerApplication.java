4882e5356b068a647978d86141b783d6f50cd838
Davessonn <babaidavid96@gmail.com>
2025-02-03 23:17:49 +0100

    Create endpoints for local tax

--- src/main/java/com/davezone/billtracker/BillTrackerApplication.java
+++ src/main/java/com/davezone/billtracker/BillTrackerApplication.java
@@ -1,10 +1,11 @@
 package com.davezone.billtracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 import java.io.Console;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class BillTrackerApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/davezone/billtracker/localtax/controller/LocalTaxController.java
+++ src/main/java/com/davezone/billtracker/localtax/controller/LocalTaxController.java
@@ -2,11 +2,15 @@ package com.davezone.billtracker.localtax.controller;
 
 import com.davezone.billtracker.localtax.model.LocalTax;
 import com.davezone.billtracker.localtax.service.LocalTaxService;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(value = "/api/localTax")
@@ -19,10 +23,34 @@ public class LocalTaxController {
         this.localTaxService = localTaxService;
     }
 
+    //Get method for querry all local taxes
     @GetMapping(value = "/all")
-    public List<LocalTax> getAllLocalTaxes() {
-        List<LocalTax> localTaxes;
-        localTaxes = localTaxService.getAllLocalTaxes();
-        return localTaxes;
+    public ResponseEntity<List<LocalTax>> getAllLocalTaxes() {
+        List<LocalTax> localTaxes = localTaxService.getAllLocalTaxes();
+        if (localTaxes.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(localTaxes);
+    }
+
+    //Get method for search local tax by id
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<LocalTax> getLocalTaxById (@PathVariable("id") long id) {
+        Optional<LocalTax> searchedLocalTax= localTaxService.getLocalTaxById(id);
+        return ResponseEntity.of(searchedLocalTax);
+    }
+
+    //Post method for create new local tax
+    @PostMapping(value = "/create")
+    public ResponseEntity<LocalTax> createLocalTax (@RequestBody LocalTax newLocalTax) {
+        if (newLocalTax == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        try {
+            LocalTax savedLocalTax = localTaxService.saveLocalTax(newLocalTax);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedLocalTax);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 }
